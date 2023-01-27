95c4e3e4c918446cc54867ce76996fb5e1588438
nayan lohiya <nayanlohiya1709@gmail.com>
2023-01-28 01:04:32 +0530

    Level 1 username and password only

---
+++ app.js
@@ -0,0 +1,73 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+// var encrypt = require('mongoose-encryption');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+mongoose.set('strictQuery',false);
+mongoose.connect('mongodb://localhost:27017/userdb' ,  {useNewUrlParser: true});
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+// const secret = "Thisisourlittlesecret.";    // this lis level2 mongoose encryption
+// userSchema.plugin(encrypt, { secret: secret , encryptedFields: ['password']});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/",function(req,res){
+  res.render("home");
+})
+app.get("/login",function(req,res){
+  res.render("login");
+})
+app.get("/register",function(req,res){
+  res.render("register");
+})
+
+app.post("/register",function(req,res){
+  const newUser = new User({
+      email: req.body.username,
+      password: req.body.password
+  });
+  newUser.save(function(err){
+  if(err){
+    console.log(err);
+  }
+  else{
+    res.render("secrets");
+  }
+  });
+});
+
+app.post("/login",function(req,res){
+  const  username = req.body.username;
+  const  password = req.body.password;
+  User.findOne(function(err,foundUser){
+  if(err){
+    console.log(err);
+  }
+  else{
+    if(foundUser){
+      if(password === foundUser.password)
+      res.render("secrets");
+    }
+  }
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
