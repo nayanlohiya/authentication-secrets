22362f6cb3a23bb85a3557ecc830e31678854e31
nayan lohiya <nayanlohiya1709@gmail.com>
2023-01-29 11:17:31 +0530

    level-6 Google Oauth 2.0 authentication

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const session = require('express-session')
-const passport = require('passport')
-const passportLocalMongoose = require('passport-local-mongoose')
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -31,22 +33,53 @@ mongoose.connect('mongodb://localhost:27017/userdb' ,  {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+done(null,user.id);
+});
 
+passport.deserializeUser(function(id, done) {
+  User.findById(id,function(err,user) {
+    done(err, user);
+  });
+});
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+    // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"      not needed
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/",function(req,res){
   res.render("home");
 });
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] })
+);
+app.get("/auth/google/secrets",
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  });
 app.get("/login",function(req,res){
   res.render("login");
 });
@@ -54,18 +87,52 @@ app.get("/register",function(req,res){
   res.render("register");
 });
 app.get("/secrets",function(req,res){
-  if(req.isAuthenticated()){
-    res.render("secrets");
+  User.find({"secret": {$ne: null}}, function(err, foundUsers){
+    if (err){
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  });
+});
+app.get("/submit", function(req, res){
+  if (req.isAuthenticated()){
+    res.render("submit");
+  } else {
+    res.redirect("/login");
   }
-  else{
-    res.render("/login");
-  }
-})
-app.get("/logout",function(req,res){
-  req.logout();
-  res.redirect("/");
-})
+});
 
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+
+//Once the user is authenticated and their session gets saved, their user details are saved to req.user.
+  // console.log(req.user.id);
+
+  User.findById(req.user.id, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
+});
+
+app.get('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) {
+      return next(err);
+      }
+    res.redirect('/');
+  });
+});  // there is change un logout check google for further details https://stackoverflow.com/questions/72336177/error-reqlogout-requires-a-callback-function
 app.post("/register",function(req,res){
   User.register({username: req.body.username}, req.body.password, function(err, user) {
     if (err) {
